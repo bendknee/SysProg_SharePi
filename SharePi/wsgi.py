40b5a284d687eb2123c3e3519adf74306e02b547
Anindito Bhagawanta <anindito.87bagas@gmail.com>
2018-12-06 03:41:22 -0700

    Add wsgi

--- SharePi/wsgi.py
+++ SharePi/wsgi.py
@@ -8,6 +8,10 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
+import time
+import traceback
+import signal
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
